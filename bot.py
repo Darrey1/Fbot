ef18ade1c7c6d1bce42a8f7963606030fa519551
Dare Timileyin <your.daretimileyin1@gimail.com>
2023-12-09 18:11:05 +0100

    b

--- bot.py
+++ bot.py
@@ -8,21 +8,22 @@ import random, re
 
 # create class object for the bot 
 class Bot(Client):   #inherit the base class Client from the fbchat
-    def __init__(self, email, password, thread_id):
-        super().__init__(email, password)
+    def __init__(self, email, password, thread_id,thread_type,cookies):
+        super().__init__(email, password, session_cookies=cookies)
         self.thread_id = thread_id
+        self.thread_type = thread_type
         
         
     def onMessage(self, author_id, message_object, thread_id, thread_type,**kwargs):
          if author_id != self.uid:
-             if (message_object.text.lower() == "is this still available?"):
+             if (message_object.text.lower().find("still available") >=0):
                 sleep(random.randrange(5,15))
                 self.markAsDelivered(thread_id, message_object.uid)
                 self.markAsRead(thread_id)
                 self.setTypingStatus(TypingStatus.TYPING, thread_id=thread_id, thread_type=thread_type)
                 sleep(random.randrange(10,25))
                 self.send(Message(text=default_message), thread_id=thread_id, thread_type=thread_type)
-             if message_object.text.find('how much') >= 0:
+             if message_object.text.lower().find('how much') >= 0:
                     sleep(random.randrange(5,15))
                     self.markAsDelivered(thread_id, message_object.uid)
                     self.markAsRead(thread_id)
@@ -30,7 +31,7 @@ class Bot(Client):   #inherit the base class Client from the fbchat
                     sleep(random.randrange(10,25))
                     self.send(Message(text=deliver_message), thread_id=thread_id, thread_type=thread_type)
                     
-             if (message_object.text.find('condition') >= 0) or (message_object.text.find('state') >= 0):
+             if (message_object.text.lower().find('condition') >= 0) or (message_object.text.find('state') >= 0):
                     sleep(random.randrange(5,15))
                     self.markAsDelivered(thread_id, message_object.uid)
                     self.markAsRead(thread_id)
@@ -38,7 +39,7 @@ class Bot(Client):   #inherit the base class Client from the fbchat
                     sleep(random.randrange(10,20))
                     self.send(Message(text=status_message), thread_id=thread_id, thread_type=thread_type)
                     
-             if (message_object.text.find('address') >= 0):
+             if (message_object.text.lower().find('address') >= 0):
                     sleep(random.randrange(5,15))
                     self.markAsDelivered(thread_id, message_object.uid)
                     self.markAsRead(thread_id)
--- main.py
+++ main.py
@@ -1,15 +1,30 @@
 from dotenv import load_dotenv
 import os
 from bot import Bot
+from fbchat.models import *
 
 load_dotenv()
 # load the crediential from the .env file
 email = os.getenv("username")  # facebook email address /username
 password = os.getenv("password")  # facebook password
 ID = os.getenv("ID")  # group id to send the message to 
+thread_type = ThreadType.USER
 
+cookies = {
+    "sb": "PdweZYhofWyHE5czZMN3_hUV",
+    "fr":"KBplOOgwTESkb9O8.AWUpYDmFSFu4kW8AZKVQdOS8nbA.BldIff.EM.AAA.0.0.BldIff.AWVjMEwC6Pc",
+    "c_user":"100083123186881",
+    "datr": "PdweZT7Gx2OiK98sDutqrcJV",
+    "xs": "4%3A9LdbLYldUQ06aQ%3A2%3A1702135760%3A-1%3A3041%3A%3AAcXggnem4xYFGT4ZSF-XQ51Ll0z5aHVLIaX1VeQa_g"
+}
 
 # creating an instance of class bot
 if __name__ == '__main__':
-    client = Bot(email, password, ID)
-    client.listen()  #loop that continuously checks for new messages
+       client = Bot(email, password, ID,thread_type,cookies)
+       print(client.isLoggedIn())
+       #client.listen()  #loop that continuously checks for new messages
+       try:
+           client.listen()
+       except Exception as arr:
+           print(arr)
+    
